1e460560fc3e7aa6d2c7e111f222ca2db7ff9f54
sadeepaghost <sadeepaamaranayake@gmail.com>
2026-09-03 21:13:08 +0530

    refactor ReportsScreen to use ScrollView for improved layout and update styles for better theming

--- mobile/src/app/(app)/reports.tsx
+++ mobile/src/app/(app)/reports.tsx
@@ -1,30 +1,49 @@
 import {
+  ScrollView,
   StyleSheet,
   Text,
-  View,
 } from "react-native";
+
+import { TestBarChart } from "@/components/charts/test-bar-chart";
 import { useTheme } from "@/hooks/use-theme";
 
 export default function ReportsScreen() {
   const theme = useTheme();
 
   return (
-    <View
-      style={[
+    <ScrollView
+      contentContainerStyle={[
         styles.container,
-        { backgroundColor: theme.background },
+        {
+          backgroundColor: theme.background,
+        },
       ]}
     >
-      <Text style={{ color: theme.text }}>
-        Reports
+      <Text
+        style={[
+          styles.heading,
+          {
+            color: theme.text,
+          }, 
+        ]}
+      >
+        Monthly report
       </Text>
-    </View>
+
+      <TestBarChart />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
+    gap: 24,
     padding: 24,
+  },
+
+  heading: {
+    fontSize: 28,
+    fontWeight: "700",
   },
 });
